d52bcd482840aecd835f719c87cd85b68c9eac9b
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-06-01 20:09:48 +0400

    Format function definition for better readability

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,8 @@ from typing import Any, Mapping, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, 
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
     Safely returns a value from a dictionary/mapping if the key exists,
     otherwise returns the default value.

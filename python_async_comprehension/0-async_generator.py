e5e1652141fb5a004bcfd050dd49b42ce4455a9b
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-06-01 20:32:18 +0400

    Refactor async_generator to yield random floats and update type hint

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-'''Async Generator'''
+"""
+Async Generator Module
+"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    '''Async Generator'''
+async def async_generator() -> Generator[float, None, None]:
+    """
+    An asynchronous generator that yields a random float between 0 and 10
+    after an asynchronous delay of 1 second, repeating 10 times.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)

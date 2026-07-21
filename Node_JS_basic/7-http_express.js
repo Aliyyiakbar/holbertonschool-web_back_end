320f09af36e6bdd17a280b7357d943bf4adf0382
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-07-21 17:09:17 +0400

    Add task 7: complex HTTP server using Express with /students endpoint

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const dbPath = process.argv[2];
+  countStudents(dbPath)
+    .then(() => {
+      res.send('This is the list of our students\n');
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;

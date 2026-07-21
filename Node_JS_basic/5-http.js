5b6567f8d22a4765ebfd0d2df617cbd32bd5f38d
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-07-21 17:13:53 +0400

    Fix task 5: capture countStudents output in HTTP response body

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,15 +6,20 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
     const dbPath = process.argv[2];
+    const originalLog = console.log;
+    const output = [];
+    console.log = (...args) => { output.push(args.join(' ')); };
     countStudents(dbPath)
       .then(() => {
-        res.end();
+        console.log = originalLog;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${output.join('\n')}`);
       })
       .catch((err) => {
-        res.end(err.message);
+        console.log = originalLog;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${err.message}`);
       });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });

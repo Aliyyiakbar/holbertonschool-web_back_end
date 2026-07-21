9303a8e799546a4dbc0b6dc8285e91ccf6b179f9
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-07-21 17:08:44 +0400

    Add task 1: process stdin to read user name and display it

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.trim();
+    if (name) {
+      console.log(`Your name is: ${name}`);
+    }
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
